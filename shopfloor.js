90581bd4205173951d6f73ba422b578a731e37eb
Matt <Matt@MATT-LAPTOP>
2018-05-13 02:37:44 -0700

    Most Features Implemented

--- shopfloor.js
+++ shopfloor.js
@@ -1,3 +1,397 @@
+	var json = [{
+			"name": "S1",
+			"nodeClass": "station1",
+			"quality" : "Q1",
+			"width" : "60",
+			"height" : "75",
+			"x": 401,
+			"y": 588
+		},
+		{
+			"name": "S2",
+			"nodeClass": "station2",
+			"quality" : "Q2",
+			"width" : "60",
+			"height" : "75",
+			"x": 460,
+			"y": 588
+		},
+		{
+			"name": "S3",
+			"nodeClass": "station3",
+			"quality" : "Q4",
+			"width" : "60",
+			"height" : "60",
+			"x": 520,
+			"y": 603
+		},
+		{
+			"name": "S4",
+			"nodeClass": "station4",
+			"quality" : "Q3",
+			"width" : "115",
+			"height" : "75",
+			"x": 782,
+			"y": 588
+		},
+		{
+			"name": "S5",
+			"nodeClass": "station5",
+			"quality" : "Q1",
+			"width" : "115",
+			"height" : "75",
+			"x": 1071,
+			"y": 588
+		},
+		{
+			"name": "S6",
+			"nodeClass": "station6",
+			"quality" : "Q4",
+			"width" : "60",
+			"height" : "60",
+			"x": 1187,
+			"y": 603
+		},
+		{
+			"name": "S7",
+			"nodeClass": "station7",
+			"quality" : "Q2",
+			"width" : "60",
+			"height" : "75",
+			"x": 1247,
+			"y": 588
+		},
+		{
+			"name": "S8",
+			"nodeClass": "station8",
+			"quality" : "Q4",
+			"width" : "60",
+			"height" : "75",
+			"x": 1308,
+			"y": 588
+		},
+		{
+			"name": "S9",
+			"nodeClass": "station9",
+			"quality" : "Q3",
+			"width" : "350",
+			"height" : "75",
+			"x": 400,
+			"y": 495
+		},
+		{
+			"name": "S10",
+			"nodeClass": "station10",
+			"quality" : "Q1",
+			"width" : "550",
+			"height" : "75",
+			"x": 986,
+			"y": 495
+		},
+			{
+			"name": "S11",
+			"nodeClass": "station11",
+			"quality" : "Q1",
+			"width" : "275",
+			"height" : "40",
+			"x": 986,
+			"y": 455
+		},
+			{
+			"name": "S12",
+			"nodeClass": "station12",
+			"quality" : "Q4",
+			"width" : "275",
+			"height" : "65",
+			"x": 986,
+			"y": 390
+		},
+			{
+			"name": "S13",
+			"nodeClass": "station13",
+			"quality" : "Q3",
+			"width" : "275",
+			"height" : "75",
+			"x": 1261,
+			"y": 421
+		},
+			{
+			"name": "S14",
+			"nodeClass": "station14",
+			"quality" : "Q1",
+			"width" : "275",
+			"height" : "31",
+			"x": 1261,
+			"y": 390
+		},
+			{
+			"name": "S15",
+			"nodeClass": "station15",
+			"quality" : "Q2",
+			"width" : "275",
+			"height" : "70",
+			"x": 986,
+			"y": 320
+		},
+			{
+			"name": "S16",
+			"nodeClass": "station16",
+			"quality" : "Q4",
+			"width" : "275",
+			"height" : "70",
+			"x": 1261,
+			"y": 320
+		},
+			{
+			"name": "S17",
+			"nodeClass": "station17",
+			"quality" : "Q3",
+			"width" : "275",
+			"height" : "70",
+			"x": 986,
+			"y": 250
+		},
+			{
+			"name": "S18",
+			"nodeClass": "station18",
+			"quality" : "Q1",
+			"width" : "275",
+			"height" : "70",
+			"x": 1261,
+			"y": 250
+		},
+			{
+			"name": "S19",
+			"nodeClass": "station19",
+			"quality" : "Q1",
+			"width" : "550",
+			"height" : "75",
+			"x": 986,
+			"y": 175
+		},
+			{
+			"name": "S20",
+			"nodeClass": "station20",
+			"quality" : "Q3",
+			"width" : "175",
+			"height" : "140",
+			"x": 400,
+			"y": 355
+		},
+			{
+			"name": "S21",
+			"nodeClass": "station21",
+			"quality" : "Q4",
+			"width" : "87.5",
+			"height" : "75",
+			"x": 575,
+			"y": 420
+		},
+			{
+			"name": "S22",
+			"nodeClass": "station22",
+			"quality" : "Q1",
+			"width" : "230",
+			"height" : "25",
+			"x": 663,
+			"y": 469
+		},
+			{
+			"name": "S23",
+			"nodeClass": "station23",
+			"quality" : "Q2",
+			"width" : "230",
+			"height" : "50",
+			"x": 663,
+			"y": 420
+		},
+			{
+			"name": "S24",
+			"nodeClass": "station24",
+			"quality" : "Q1",
+			"width" : "318",
+			"height" : "65",
+			"x": 575,
+			"y": 355
+		},
+			{
+			"name": "S25",
+			"nodeClass": "station25",
+			"quality" : "Q2",
+			"width" : "318",
+			"height" : "70",
+			"x": 575,
+			"y": 286
+		},
+			{
+			"name": "S26",
+			"nodeClass": "station26",
+			"quality" : "Q1",
+			"width" : "493",
+			"height" : "112",
+			"x": 400,
+			"y": 175
+		},
+		{
+			"name": "S27",
+			"nodeClass": "station27",
+			"quality" : "Q4",
+			"width" : "350",
+			"height" : "120",
+			"x": 48,
+			"y": 14
+		},
+		{
+			"name": "S28",
+			"nodeClass": "station28",
+			"quality" : "Q2",
+			"width" : "246.5",
+			"height" : "120",
+			"x": 398,
+			"y": 14
+		},
+		{
+			"name": "S29",
+			"nodeClass": "station29",
+			"quality" : "Q1",
+			"width" : "246.5",
+			"height" : "120",
+			"x": 645,
+			"y": 14
+		},
+		{
+			"name": "S30",
+			"nodeClass": "station30",
+			"quality" : "Q2",
+			"width" : "307",
+			"height" : "120",
+			"x": 1230,
+			"y": 14
+		}
+];
+
+var create_node_modal_active = false;
+var rename_node_modal_active = false;
+var show_node_details_active = false;
+var node_to_rename = null;
+var admin_mode = false;
+
+function set_admin_on() {
+    admin_mode = true;
+    alert("Admin Mode On");
+}
+
+function close_modal() {
+    $(document).foundation('reveal', 'close');
+}
+
+function create_node() {
+    if(admin_mode) {
+        if (create_node_modal_active) {
+                name = $('#CreateNodeName').val();
+                new_node = { 'name': name,
+							 'quality' : "Q1",
+                             'width' : "100",
+							 'nodeClass' : "newStation",
+							'height' : "100",
+							'x': 1000,
+							'y': 100
+                           };
+			var station = d3.select("#charts")
+            .append("svg:g")
+                .data([ {"x": 1000, "y": 100} ])
+                .attr("class", "newNode")
+               .attr("transform", "translate(" +1000 + "," + 100 + ")")
+                .call(drag)
+				.on('contextmenu', d3.contextMenu(menu))
+				.on('click', function(d){
+				var modal = $('#ShowNodeDetailsModal')
+				show_node_details_active = true;
+				$('#ShowNodeDetailsModal').foundation('reveal', 'open')});
+
+        console.log("make node");
+
+        var nodeData = station.append("svg:rect")
+                .attr("class", "nodeRect")
+                .attr("fill", "red")
+                .attr("fill-opacity", .8)
+                .attr("stroke-width", 1)
+				.attr("stroke", "#000")
+				.attr("x", -24)
+				.attr("y", -12)
+				.attr('width', 100)
+				.attr('height', 100)
+      
+        var text = station.append("svg:text")
+            .text(name)
+            .attr("y", ".5em")
+            .attr("text-anchor", "middle")
+            .attr("font-weight", 700)
+            .attr("font-family", "Helvetica")
+            .attr("fill", "#000000")
+            .attr("stroke", "none")
+            .attr("pointer-events", "none") 
+
+            console.log('Create Node name: ' + name);
+            create_node_modal_active = false;
+            $('#CreateNodeName').val('');
+			close_modal();
+        }
+	}
+  else {
+	  alert("Need to be an Admin");
+	   close_modal();
+		}
+ 
+}
+
+var menu = [
+                {
+                        title: 'Edit node',
+                        action: function(elm, d, i) {
+                                console.log('Edit node');
+                                $("#RenameNodeName").val(d.name);
+                                rename_node_modal_active = true;
+                                node_to_rename = d
+                                $("#RenameNodeName").focus();
+                                $('#RenameNodeModal').foundation('reveal', 'open');
+                        }
+                },
+                {
+                        title: 'Delete node',
+                        action: function(elm, d, i) {
+                                console.log('Delete node');
+                                delete_node(d);
+                        }
+                },
+                {
+                        title: 'Create child node',
+                        action: function(elm, d, i) {
+                                console.log('Create child node');
+                                create_node_parent = d;
+                                create_node_modal_active = true;
+                                $('#CreateNodeModal').foundation('reveal', 'open');
+                                $('#CreateNodeName').focus();
+                        }
+                }
+        ]
+
+  function colorNode(d) {
+        if (d.quality == "Q1") {
+            result = "deepskyblue"
+          } else if (d.quality == "Q2") {
+            result = "springgreen"
+          } else if (d.quality == "Q3") {
+            result = "yellow"
+          } else if (d.quality ==  "Q4") {
+			  result = "crimson"
+		  }
+		  else {
+			  result = "lightsteelblue"
+		  }
+        return result;
+    }
+
 var drag = d3.behavior.drag()
         .on("drag", function(d,i) {
             d.x += d3.event.dx
@@ -8,80 +402,46 @@ var drag = d3.behavior.drag()
         });
 
 
-    function face_factory(classname, data, x, y, r)
+		
+		
+	function plotNodes()
     { 
-        //data format
-        //{ "x": 0 - 1 , "y": 0 -1, "z": 0-1 }
-        //color could be made a parameter
-        //var arc = d3.svg.arc().outerRadius(r)
-        //var donut = d3.layout.pie();
 
-        var face = d3.select("#charts")
+	for(var i = 0; i < json.length; i++) {
+		var node = json[i];
+        var station = d3.select("#charts")
             .append("svg:g")
-                //.data([data.sort(d3.descending)])
-                //.data([data])
-                .data([ {"x":x, "y":y} ])
-                .attr("class", classname)
-                .attr("transform", "translate(" + x + "," + y + ")")
-                .call(drag);
+                .data([ {"x":node.x, "y":node.y} ])
+                .attr("class", node.nodeClass)
+               .attr("transform", "translate(" + node.x + "," + node.y + ")")
+                .call(drag)
+				.on('contextmenu', d3.contextMenu(menu))
+				.on('click', function(d){
+				var modal = $('#ShowNodeDetailsModal')
+				show_node_details_active = true;
+				$('#ShowNodeDetailsModal').foundation('reveal', 'open')});
 
 
-        console.log("make head");
-        var head_color = d3.scale.linear()
+        console.log("make node");
+        var node_color = d3.scale.linear()
             .domain([0, 1])
             .interpolate(d3.interpolateRgb)
             .range(["#ff0000", "#0000ff"]);
 
-        var head = face.append("svg:rect")
+        var nodeData = station.append("svg:rect")
                 .attr("class", "nodeRect")
-                .attr("fill", function(d){ return head_color(data.x); })
+                .attr("fill", colorNode(node))
                 .attr("fill-opacity", .8)
-                .attr("stroke", "#000")
-                .attr("stroke-width", 2)
+                .attr("stroke-width", 1)
+				.attr("stroke", "#000")
 				.attr("x", -24)
 				.attr("y", -12)
-				.attr('width', 50)
-				.attr('height', 50)
-                //.attr("r", r);
-
-        console.log("make mouth");
-        var mouth_x = [0, .5, 1];
-
-        var mouth_x_range = d3.scale.linear()
-            .domain([0, 1])
-            .range([-r/2, r/2]);
-
-
-        var mouth_y_range = d3.scale.linear()
-            .domain([0, 1])
-            .range([-r/2, r/2]);
-
-        var mouth_y = [.5, data.y, .5];
-        console.log(mouth_y)
-
-        var mouth_line = d3.svg.line()
-            .x(function(d,i) {
-                return mouth_x_range(mouth_x[i]);
-            })
-            .y(function(d,i) {
-                return mouth_y_range(mouth_y[i]);
-            })
-            .interpolate("basis");
-
-        /*
-        var mouth = face.append("svg:path")
-                .attr("class", "mouth")
-                .attr("stroke", "#000")
-                .attr("stroke-width", 4)
-                .attr("fill", "none")
-                .attr("transform", "translate(" + [0, r/3] + ")")
-                .attr("d", mouth_line(mouth_x));
-                */
-
-        var text = face.append("svg:text")
-            .text("QA")
+				.attr('width', node.width)
+				.attr('height', node.height)
+      
+        var text = station.append("svg:text")
+            .text(node.name)
             .attr("y", ".5em")
-            .attr("transform", "translate(" + [0, r/3] + ")")
             .attr("text-anchor", "middle")
             .attr("font-weight", 700)
             .attr("font-family", "Helvetica")
@@ -89,9 +449,11 @@ var drag = d3.behavior.drag()
             .attr("stroke", "none")
             .attr("pointer-events", "none") 
 
-    }
-
-
+		}
+		
+		 
+		 
+	}
 	var viewerWidth = $(document).width();
     var viewerHeight = $(document).height();
 
@@ -105,18 +467,7 @@ var drag = d3.behavior.drag()
             .append("svg:rect")
                 .attr("width", "100%")
                 .attr("height", "100%")
-                .attr("stroke", "#000")
-                .attr("stroke-width", 3)
                 .attr("fill", "none")
 
-    //r = 100;
-    for(i = 0; i < 20; i++)
-    {
-        var r = 20 + Math.random() * 50;
-        var data = { "x":Math.random(), "y":Math.random(), "z":Math.random(), "w":Math.random() };
-        x = Math.random() * viewerWidth
-        y = Math.random() * viewerHeight
-        face_factory("face"+i, data, x, y, r);
-    }
-    
+
    
